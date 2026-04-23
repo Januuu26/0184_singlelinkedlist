2ca990fff9be2984d9634064ed7e4e44bfcbb6cf
Januuu26 <b77286116@gmail.com>
2026-04-23 18:10:19 +0700

    membuat library

---
+++ SingleLinked_ist.cpp
@@ -0,0 +1,57 @@
+#include  <iostream>
+#include  <string>
+using namespace std;
+
+class Node
+{
+public:
+    int noMhs;
+    Node *next;
+};
+
+class LinkedList
+{   
+    Node *START;
+
+public:
+    LinkedList()
+    {
+        START = NULL;
+    }
+
+    void addNode();
+{
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa : ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node();
+        nodeBaru->noMhs = nim;
+
+        if (START === NULL || nim <= START ->noMhs)
+        {
+            if (START != NULL && nim <= START->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak dijalankan\n";
+                return;
+            }
+
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;
+
+
+        while (current != NULL && nim >= current->noMhs)
+        {
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
+
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
+}
